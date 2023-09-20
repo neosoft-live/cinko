90f8825da859a4663a3f714d4e8245a6c98dc798
shrikantbucket <shrikant.codebag@gmail.com>
2023-09-20 18:24:28 +0530

    update the router setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cinko';
+
+
+  showFooter:boolean = true;
+  showHeader:boolean = true;
+
+
+  constructor(private router:Router){
+      router.events.subscribe((val)=>{
+        if(val instanceof NavigationEnd){
+          if(val.url == '/add-new-hotel' || val.url == "/dashboard" ||
+          val.url == '/view-hotel' || val.url == "/hotels-list" ||
+          val.url == '/hotel-booking' || val.url == "/hotel-invoice" ||
+          val.url == '/hotel-ticket' || val.url == "/my-room" ||
+          val.url == '/view-room' || val.url == "/view-tickets" ||
+          val.url == '/user-profile'
+          ){
+            this.showHeader = false;
+            this.showFooter = false;
+          }else{
+            this.showHeader = true;
+            this.showFooter = true;
+          }
+        }
+      })
+  }
+
 }
